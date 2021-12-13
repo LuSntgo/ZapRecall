fac9d952d0d78319816d9fb5cbab2d4b75642b10
Luiza Sntg <lu.sntgo@gmail.com>
2021-12-13 14:40:52 -0300

    Adicionado bonus na SucessPage e FailPage

--- src/components/FailurePage/Fail.js
+++ src/components/FailurePage/Fail.js
@@ -1,5 +1,6 @@
 import './style.css';
 import sad from "../../assets/sad.png";
+import { Link } from "react-router-dom";
 
 export default function Fail() {
     return (
@@ -12,6 +13,12 @@ export default function Fail() {
         <p class="textFail">Você esqueceu alguns flashcards... 
         <br />
         Não desanime! Na próxima você consegue!</p>
+        <Link to="/">
+        <button data-identifier="start-zap-recall" class="restartCard">
+          {" "}
+          Tentar novamente{" "}
+        </button>
+        </Link>
         </div>
         </div>
         </>
--- src/components/SuccessPage/Success.js
+++ src/components/SuccessPage/Success.js
@@ -1,5 +1,6 @@
 import './style.css';
 import happy from "../../assets/party.png";
+import { Link } from "react-router-dom";
 
 export default function Success() {
     return (
@@ -10,6 +11,12 @@ export default function Success() {
         <img class="happy" src={happy}></img>
         </h1>
         <p class="textSuccess">Você não esqueceu de nenhum flashcard!!</p>
+        <Link to="/">
+        <button data-identifier="start-zap-recall" class="restartCard">
+          {" "}
+          Tentar novamente{" "}
+        </button>
+        </Link>
         </div>
         </div>
         </>
