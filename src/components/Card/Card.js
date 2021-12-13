426b3bcacc39b7da8981b78c1ef0e24e9c4263a1
Luiza Sntg <lu.sntgo@gmail.com>
2021-12-12 23:30:19 -0300

    Adicionado borda ao clicar no botao e contador de cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,45 +7,59 @@ export default function Card(props) {
   const [isFront, setFrontCard] = useState(true);
   const [cardCounter, setCard] = useState(0);
   const { question, answer } = text[cardCounter];
-  const [style, setStyle] = useState("back-card")
+  const [style, setStyle] = useState("back-card");
+  const [isAnswered, setAnswered] = useState(false)
 
-  const ChangeStyleBlack =() => {setStyle("borderColorBlack");}
-  const ChangeStyleRed =() => {setStyle("borderColorRed");}
-  const ChangeStyleGreen =() => {setStyle("borderColorGreen");}
-  const ChangeStyleYellow =() => {setStyle("borderColorYellow");}
-
+  function ChangeStyle(color) {
+    setStyle(`borderColor${color}`);
+    setAnswered(true)
+  }
   function TryReact() {
     setCard(cardCounter + 1);
+    setAnswered(false)
     setFrontCard(true);
+    setStyle("");
   }
 
   function Flip() {
     setFrontCard(false);
   }
- 
- 
+
   return (
-    <>        {
-      isFront ? 
-      <div class="front-card">
-        <p class="counter">{cardCounter + 1}/8</p>
-        <h1 class="title">{question}</h1>
-        <img class="turn" src={turn} onClick={() => Flip()} alt="turn" />
-      </div>
-        : 
-      <div className={style}>
-      <div class="back-card">
-        <p class="counter">{cardCounter +1}/8</p>
-        <h1 class="title-mini">{question}</h1>
-        <h3 class="answer">{answer}</h3>
-        <div class="buttons">
-        <button class="black" onClick={() => ChangeStyleBlack()}>"Aprendi agora"</button>
-        <button class="red" onClick={() => ChangeStyleRed()}>"Não lembrei"</button>
-        <button class="green" onClick={() => ChangeStyleGreen()}>"Lembrei com esforço"</button>
-        <button class="yellow" onClick={() => ChangeStyleYellow()}><strong>"Zap!"</strong></button>
+    <>
+      {" "}
+      {isFront ? (
+        <div class="front-card">
+          <p class="counter">{cardCounter + 1}/8</p>
+          <h1 class="title">{question}</h1>
+          <img class="turn" src={turn} onClick={() => Flip()} alt="turn" />
         </div>
+      ) : (
+        <div className={style}>
+          <div class="back-card">
+            <p class="counter">{cardCounter + 1}/8</p>
+            <h1 class="title-mini">{question}</h1>
+            <h3 class="answer">{answer}</h3>
+            {isAnswered ? <img class="turn" src={turn} onClick={() => TryReact()} alt="turn" /> : (
+            <div class="buttons">
+              <button class="black" onClick={() => ChangeStyle("Black")}>
+                "Aprendi agora"
+              </button>
+              <button class="red" onClick={() => ChangeStyle("Red")}>
+                "Não lembrei"
+              </button>
+              <button class="green" onClick={() => ChangeStyle("Green")}>
+                "Lembrei com esforço"
+              </button>
+              <button class="yellow" onClick={() => ChangeStyle("Yellow")}>
+                <strong>"Zap!"</strong>
+              </button>
+              
+            </div>
+              )}
+          </div>
         </div>
-      </div>}
+      )}
     </>
   );
 }
--- src/components/Failure/Fail.js
+++ src/components/Failure/Fail.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+export default Fail() {
+    return (
+        <>
+        <div class="back-card">
+        <p class="counter">{cardCounter +1}/8</p>
+        <h1 class="title-mini">{question}</h1>
+        <h3 class="answer">{answer}</h3>
+        </div>
+        </>
+
+    )
+}
