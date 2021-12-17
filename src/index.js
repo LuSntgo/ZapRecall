8732d6d0eea86847f935a1cc005f2806369c5735
mrcs22 <marcusmoraes2010@hotmail.com>
2021-12-17 16:47:51 -0300

    Preparando revisão

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM  from "react-dom";
-import React, {useState} from "react";
-import App from "./App";
-
-
-const root = document.querySelector(".root");
-ReactDOM.render(<App />, root);
-
-
